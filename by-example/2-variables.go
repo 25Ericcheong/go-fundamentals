46f96f68f0b393abefd7a394761b3f0c12ca6195
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-13 20:22:34 +1000

    notes about zero valued and shorthand for initialization of a variable

--- by-example/2-variables.go
+++ by-example/2-variables.go
@@ -13,9 +13,13 @@ func main() {
 	var d = true
 	fmt.Println(d)
 
+	// zero value for an int is 0
 	var e int
 	fmt.Println(e)
 
+	// shorthand way for declaring and initializng a variable
+	// in this case this is equivalent to
+	// var f string = "aooke"
 	f := "apple"
 	fmt.Println(f)
 }
