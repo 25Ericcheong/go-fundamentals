6b9d3a887dbc1cfc7d64eec0385546323e94ea23
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-18 11:37:46 +1000

    extended requirements - included more tests and updated function

--- 0-hello-world/hello.go
+++ 0-hello-world/hello.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello"
+const helloEnglishPrefix = "Hello"
+const helloMandarinPrefix = "Ni Hao"
+const helloFrenchPrefix = "Bonjour"
+const english = "English"
+const french = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name != "" {
-		return fmt.Sprintf("%v %v, from a Function!", helloPrefix, name)
+
+		if language == english {
+			return fmt.Sprintf("%v %v, from a Function!", helloEnglishPrefix, name)
+		} else if language == french {
+			return fmt.Sprintf("%v %v, from a Function!", helloFrenchPrefix, name)
+		} else {
+			return fmt.Sprintf("%v %v, from a Function!", helloMandarinPrefix, name)
+		}
 	}
 	return "Hello Nobody!"
 }
 
 func main() {
-	fmt.Println(Hello("Eric"))
+	fmt.Println(Hello("Eric", ""))
 }
--- 0-hello-world/hello_test.go
+++ 0-hello-world/hello_test.go
@@ -3,15 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("Function is saying hello to someone", func(t *testing.T) {
-		got := Hello("Eric")
+	t.Run("Function is saying Hello to someone", func(t *testing.T) {
+		got := Hello("Eric", "English")
 		want := "Hello Eric, from a Function!"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Function saying Hello when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello Nobody!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Function saying Hello to someone in mandarin", func(t *testing.T) {
+		got := Hello("Eric", "Mandarin")
+		want := "Ni Hao Eric, from a Function!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Function saying Hello to someone in french", func(t *testing.T) {
+		got := Hello("Eric", "French")
+		want := "Bonjour Eric, from a Function!"
 		assertCorrectMessage(t, got, want)
 	})
 }
