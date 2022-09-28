b2a6f56f35e5bc56ec3c1bc2e323ca71f99d2ed2
25Ericcheong <ericcheongkaikit@gmail.com>
2022-09-28 21:43:18 +1000

    added failling test

---
+++ 7-dependency-injection/greet_test.go
@@ -0,0 +1,19 @@
+package dependencyinject
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	// Buffer type from bytes implements Write  interface because
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
