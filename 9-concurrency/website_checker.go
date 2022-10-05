eb129060b5faceec3b19ad8cd7178747d085fa4c
25Ericcheong <ericcheongkaikit@gmail.com>
2022-10-05 21:13:45 +1000

    handling concurrency the right way to control timing of addition to map

--- 9-concurrency/website_checker.go
+++ 9-concurrency/website_checker.go
@@ -2,13 +2,30 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+// made to associate itself with the return value of WebsiteChecker
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		go func(u string) {
-			results[u] = wc(u)
+			// send statement
+			// send values to the channel
+			resultChannel <- result{u, wc(u)}
 		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		// receive expression
+		// assign values that we have received (inserted into the channel) to a variable
+		// then use the variable to update the results
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 
 	return results
