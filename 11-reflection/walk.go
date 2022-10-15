51248cbd0f645bcf938d292e777ffe379f7c414a
25Ericcheong <ericcheongkaikit@gmail.com>
2022-10-15 10:54:32 +1000

    refactor code to use switch statement instead

--- 11-reflection/walk.go
+++ 11-reflection/walk.go
@@ -8,12 +8,10 @@ func walk(x interface{}, fn func(input string)) {
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		}
-
-		// recursion where we send the struct within the struct to walk again along with the same fn we used initiallys
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			walk(field.Interface(), fn)
 		}
 	}
