6e450fa972646ef8ffa60e49184427091ff204a4
25Ericcheong <ericcheongkaikit@gmail.com>
2022-10-15 10:31:39 +1000

    refactored code to use range instead to test for different scenarios and added additional comments to better understand what is happenning

--- 11-reflection/walk_test.go
+++ 11-reflection/walk_test.go
@@ -1,25 +1,41 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
 
-	x := struct {
-		Name string
-	}{expected}
-
-	// passed an anonymous func as second parameter into walk
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	// to test for multiple different scenarios
+	// an array containing an anon struct type with the following fields
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
 	}
 
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+
+			// remember, walk has already been implemented which utilizes the anonymous func to acquire the any type - interface{}'s field called Name (assuming we only have one field) that has a string type (assuming it is a string type) on Runtime.
+			// this anonymous func essentially takes the Field mentioned above as an argument/parameter called input (we need to test that the function has successfully acquire such string which is why we store the value to check if the right string has been sent through)
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
