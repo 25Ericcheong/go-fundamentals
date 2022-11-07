e1d5c1e27fbbcdfaec2060a8d5a4e6f42e8726d9
25Ericcheong <ericcheongkaikit@gmail.com>
2022-11-07 21:35:02 +1000

    code to convert arabic num:

--- 14-property-based-test/roman_numerals.go
+++ 14-property-based-test/roman_numerals.go
@@ -37,10 +37,63 @@ func ConvertToRoman(arabic int) string {
 	return result.String()
 }
 
+type RomanNumerals []RomanNumeral
+
+var allRomanNumeralsTyped = RomanNumerals{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
 func ConvertToArabic(roman string) int {
 	total := 0
-	for range roman {
-		total++
+
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		// look ahead to next symbol if can and, the current symbol is base 10 (only valid subtractors)
+		if couldBeSubtractive(i, symbol, roman) {
+			nextSymbol := roman[i+1]
+
+			// build the two character string
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			// get value of two character string
+			value := allRomanNumeralsTyped.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++ // move past this character too for the next loop
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
 	}
 	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	return index+1 < len(roman) && currentSymbol == 'I'
 }
