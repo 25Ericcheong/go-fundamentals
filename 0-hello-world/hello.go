e04e0c51f4aa032027852d05c8b3ccaba6f72b7c
25Ericcheong <ericcheongkaikit@gmail.com>
2023-04-18 22:54:00 +1000

    decided to restart fundamentals to refresh memory

--- 0-hello-world/hello.go
+++ 0-hello-world/hello.go
@@ -2,32 +2,10 @@ package main
 
 import "fmt"
 
-const helloEnglishPrefix = "Hello"
-const helloMandarinPrefix = "Ni Hao"
-const helloFrenchPrefix = "Bonjour"
-const english = "English"
-const french = "French"
-
-func Hello(name string, language string) string {
-	if name != "" {
-		helloPrefix := greetingPrefix(language)
-		return fmt.Sprintf("%v %v, from a Function!", helloPrefix, name)
-	}
-	return "Hello Nobody!"
-}
-
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case french:
-		prefix = helloFrenchPrefix
-	case english:
-		prefix = helloEnglishPrefix
-	default:
-		prefix = helloMandarinPrefix
-	}
-	return
+func Hello() string {
+	return "Hello, world"
 }
 
 func main() {
-	fmt.Println(Hello("Eric", ""))
+	fmt.Println(Hello())
 }
--- 0-hello-world/hello_test.go
+++ 0-hello-world/hello_test.go
@@ -3,34 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("Function is saying Hello to someone", func(t *testing.T) {
-		got := Hello("Eric", "English")
-		want := "Hello Eric, from a Function!"
-		assertCorrectMessage(t, got, want)
-	})
+	got := Hello()
+	want := "Hello, world"
 
-	t.Run("Function saying Hello when an empty string is supplied", func(t *testing.T) {
-		got := Hello("", "")
-		want := "Hello Nobody!"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("Function saying Hello to someone in mandarin", func(t *testing.T) {
-		got := Hello("Eric", "Mandarin")
-		want := "Ni Hao Eric, from a Function!"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("Function saying Hello to someone in french", func(t *testing.T) {
-		got := Hello("Eric", "French")
-		want := "Bonjour Eric, from a Function!"
-		assertCorrectMessage(t, got, want)
-	})
-}
-
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
 	if got != want {
-		t.Errorf("Got %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
