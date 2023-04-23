c464e02b90905ebbdf26b09565b5872490165ff0
25Ericcheong <ericcheongkaikit@gmail.com>
2023-04-23 21:57:47 +1000

    able to create local package to be reused across multiple modules

---
+++ 0-hello-world/assert/check.go
@@ -0,0 +1,10 @@
+package assert
+
+import "testing"
+
+func CheckCorrectValue(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
--- 0-hello-world/hello_test.go
+++ 0-hello-world/hello_test.go
@@ -1,38 +1,36 @@
 package main
 
-import "testing"
+import (
+	"0-hello-world/assert"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people in English", func(t *testing.T) {
 		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
-		assertCorrectMessage(t, got, want)
+		assert.CheckCorrectValue(t, got, want)
 	})
 
 	t.Run(("in Spanish"), func(t *testing.T) {
 		got := Hello("Elodie", "Spanish")
 		want := "Hola, Elodie"
-		assertCorrectMessage(t, got, want)
+
+		assert.CheckCorrectValue(t, got, want)
 	})
 
 	t.Run(("in French"), func(t *testing.T) {
 		got := Hello("Eric", "French")
 		want := "Bonjour, Eric"
-		assertCorrectMessage(t, got, want)
+
+		assert.CheckCorrectValue(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
 
-		assertCorrectMessage(t, got, want)
+		assert.CheckCorrectValue(t, got, want)
 	})
-}
-
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
 }
