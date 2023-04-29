628cbede800f2d4d488b6ffac01b3e88d6a9965b
25Ericcheong <ericcheongkaikit@gmail.com>
2023-04-30 09:05:16 +1000

    changed from fixed array size to slice type and updated test and existing function

--- 3-arrays-and-slices/sum_test.go
+++ 3-arrays-and-slices/sum_test.go
@@ -3,16 +3,25 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	CheckValue(t, got, want, numbers)
-}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
-func CheckValue(t testing.TB, got, want int, input [5]int) {
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, input)
-	}
+	t.Run(("collection of any size"), func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
