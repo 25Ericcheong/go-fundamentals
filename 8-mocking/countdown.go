a55b16b8ccce9bcc69aa73be719fc863ce1a9d0a
25Ericcheong <ericcheongkaikit@gmail.com>
2023-05-01 23:10:08 +1000

    print simple 3 and tested printing via dependency injection

--- 8-mocking/countdown.go
+++ 8-mocking/countdown.go
@@ -4,35 +4,12 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"time"
 )
 
-type Sleeper interface {
-	Sleep()
+func Countdown(out io.Writer) {
+	fmt.Fprint(out, "3")
 }
-
-type ConfigurableSleeper struct {
-	duration time.Duration
-	sleep    func(time.Duration)
-}
-
-func (c *ConfigurableSleeper) Sleep() {
-	c.sleep(c.duration)
-}
-
-const finalWord = "Go!"
-const countdownStart = 3
 
 func main() {
-	sleeper := &ConfigurableSleeper{2 * time.Second, time.Sleep}
-	Countdown(os.Stdout, sleeper)
-}
-
-func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
-		fmt.Fprintln(out, i)
-		sleeper.Sleep()
-	}
-
-	fmt.Fprint(out, finalWord)
+	Countdown(os.Stdout)
 }
