f719d8b9cc968782d4e57c27f25db61cb0fc32fb
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-25 17:48:29 +1000

    completed sync code with additional notes for reference in the future

--- 12-sync/counter.go
+++ 12-sync/counter.go
@@ -1,13 +1,18 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	sync.Mutex
 	value int
 }
 
+// this means that when each go routine (in our case; a 1000) calls this method - it must acquire a lock first and all other go routines will have to wait for it to be unlocked before getting access
 func (c *Counter) Inc() {
+	c.Lock()
+	defer c.Unlock()
 	c.value++
 }
-
 func (c *Counter) Value() int {
 	return c.value
 }
