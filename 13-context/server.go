b19cc747b2bb5febbcd040096f3e7b4284eaa5d3
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-29 21:20:15 +1000

    before change where we pass context into server and allow it to be responsible for cancelling a process with context

--- 13-context/server.go
+++ 13-context/server.go
@@ -12,7 +12,20 @@ type Store interface {
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		// race two asynchronous processes with select
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
