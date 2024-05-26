e164aa1e8504d99827f20ef1c4a730374c664453
25Ericcheong <ericcheongkaikit@gmail.com>
2024-05-26 17:44:45 +0800

    feat: Return err if name is empty and configured log to print error message

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 	"go-fundamentals/greetings"
+	"log"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
