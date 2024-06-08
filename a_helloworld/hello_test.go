c07b77fde835fcafb03557e9f9c5aefe646db90f
25Ericcheong <ericcheongkaikit@gmail.com>
2024-06-08 15:17:07 +0800

    feat(a): Used constant variable and updated tests

--- a_helloworld/hello_test.go
+++ a_helloworld/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Hi Eric")
-	want := "Hi Eric"
+	got := Hello("Eric")
+	want := "Hello, Eric"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
