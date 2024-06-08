d61a0f82158e8ae0d540c9f27cecf2cbc64fbd77
25Ericcheong <ericcheongkaikit@gmail.com>
2024-06-08 15:21:44 +0800

    feat(a): Instance with no param for hello world and updated test

--- a_helloworld/hello_test.go
+++ a_helloworld/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Eric")
-	want := "Hello, Eric"
+	t.Run("saying hello with name", func(t *testing.T) {
+		got := Hello("Eric")
+		want := "Hello, Eric"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("saying hello without name should print 'Hello, World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
