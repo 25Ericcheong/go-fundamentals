cd37e8f74c95db5458e6c393632462f1a4d984c8
25Ericcheong <ericcheongkaikit@gmail.com>
2024-06-15 15:19:01 +0800

    feat: Use interfaces that implicitly refers structs with same method signature

--- e_structs_methods_and_interfaces/shape.go
+++ e_structs_methods_and_interfaces/shape.go
@@ -2,6 +2,10 @@ package structs_methods_and_interfaces
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -11,14 +15,14 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
-}
-
-func Perimeter(rect Rectangle) float64 {
-	return 2 * (rect.Width + rect.Height)
 }
