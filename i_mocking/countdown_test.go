987fa702ad3c858521998b71fa0fa6aa939ffd32
25Ericcheong <ericcheongkaikit@gmail.com>
2024-07-08 13:58:21 +0800

    feat: Cool mocking concept to test the order of methods being called by extending existing spy to be able to implement multiple interfaces and then track when both methods are called

--- i_mocking/countdown_test.go
+++ i_mocking/countdown_test.go
@@ -3,26 +3,49 @@ package main
 import (
 	"bytes"
 	"go-fundamentals/utils"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
 
-	Countdown(buffer, spySleeper)
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
 
-	got := buffer.String()
-	want := `3
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	utils.AssertCorrectStringsMessage(t, got, want)
+		utils.AssertCorrectStringsMessage(t, got, want)
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
-	}
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
 }
 
 // Spies are a kind of mock to record how dependencies are used
@@ -33,3 +56,19 @@ type SpySleeper struct {
 func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
