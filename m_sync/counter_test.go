4c58fbd4e9cbc5c68a9c212a231cd815a0defe60
25Ericcheong <ericcheongkaikit@gmail.com>
2024-07-08 21:50:32 +0800

    feat: Used mutex which ensure tests are not flaky

--- m_sync/counter_test.go
+++ m_sync/counter_test.go
@@ -5,26 +5,28 @@ import (
 	"testing"
 )
 
-t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 
-	t.Run("it runs safely concurrently", func(t *testing.T) {
-		wantedCount := 1000
-		counter := Counter{}
+		t.Run("it runs safely concurrently", func(t *testing.T) {
+			wantedCount := 1000
+			counter := Counter{}
 
-		var wg sync.WaitGroup
-		wg.Add(wantedCount)
+			var wg sync.WaitGroup
+			wg.Add(wantedCount)
 
-		for i := 0; i < wantedCount; i++ {
-			go func() {
-				counter.Inc()
-				wg.Done()
-			}()
-		}
-		wg.Wait()
+			for i := 0; i < wantedCount; i++ {
+				go func() {
+					counter.Inc()
+					wg.Done()
+				}()
+			}
+			wg.Wait()
 
-		assertCounter(t, counter, wantedCount)
+			assertCounter(t, counter, wantedCount)
+		})
 	})
-})
+}
 
 func assertCounter(t testing.TB, got Counter, want int) {
 	t.Helper()
