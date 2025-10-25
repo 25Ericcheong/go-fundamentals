1ec54ff06679e23caa4c2cd0baebc99522c5f73f
25Ericcheong <ericcheongkaikit@gmail.com>
2025-10-25 21:48:35 +0800

    task: Use interface called shape to represent rectangle and circle and updated tests

--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -14,23 +14,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("rectangles", func(t *testing.T) {
-		rect := Rectangle{Width: 12, Height: 6}
-		got := rect.Area()
-		want := 72.0
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{Width: 12, Height: 6}
+		checkArea(t, rect, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
