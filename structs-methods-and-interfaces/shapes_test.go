b3e6096e778ea7b46e3fe0dbdf2d54b40e4ca25b
25Ericcheong <ericcheongkaikit@gmail.com>
2025-10-25 21:55:08 +0800

    task: New triangle struct type and added new tests

--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -14,21 +14,42 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	//checkArea := func(t testing.TB, shape Shape, want float64) {
+	//	t.Helper()
+	//	got := shape.Area()
+	//	if got != want {
+	//		t.Errorf("got %g want %g", got, want)
+	//	}
+	//}
+
+	// using table driven tests below with struct anonymous type and adding name to identify each test name uniquely
+	//t.Run("rectangles", func(t *testing.T) {
+	//	rect := Rectangle{Width: 12, Height: 6}
+	//	checkArea(t, rect, 72.0)
+	//})
+	//
+	//t.Run("circles", func(t *testing.T) {
+	//	circle := Circle{10}
+	//	checkArea(t, circle, 314.1592653589793)
+	//})
+
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{"Rectangle", Rectangle{Width: 12, Height: 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rect := Rectangle{Width: 12, Height: 6}
-		checkArea(t, rect, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		// use tt.name from case to use as a test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want %g", got, tt.want)
+			}
+		})
+	}
 }
